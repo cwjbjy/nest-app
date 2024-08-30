c2f0ba2beb0133510b3b07d682c4d2b9657ce511
cwjbjy <595842570@qq.com>
2024-08-30 13:12:55 +0800

    feat(全局配置): 托管静态文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,25 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import configuration from 'src/core/config';
 import { JwtAuthGuard } from 'src/core/guards/auth.guard';
 import { RolesGuard } from 'src/core/guards/roles.guard';
 
 import { AuthModule } from './module/auth/auth.module';
 import { UserModule } from './module/user/user.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       cache: true, //缓存
       load: [configuration], //加载配置文件
       isGlobal: true, //设置为全局
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/static', //不添加，只能读取html文件
     }),
     AuthModule,
     UserModule,
