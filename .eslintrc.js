3db89b9c503c0e6fb4d1f179f591bd1167e79b04
cwjbjy <595842570@qq.com>
2024-08-30 09:41:10 +0800

    feat: 增加ESLint import规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin','import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,6 +21,30 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    "prettier/prettier": ["error", { "endOfLine": "auto" }]
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'import/order': [
+      'error',
+      {
+        //按照分组顺序进行排序
+        groups: [
+          'builtin',
+          'external',
+          'parent',
+          'sibling',
+          'index',
+          'internal',
+          'object',
+          'type',
+        ],
+        //将@nestjs/common包不进行排序，并放在前排，可以保证放在第一行
+        pathGroupsExcludedImportTypes: ['@nestjs/common'],
+        'newlines-between': 'always', //每个分组之间换行
+        //根据字母顺序对每个组内的顺序进行排序
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
 };
